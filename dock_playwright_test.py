d01287f41ddbcbf54eee6b1badd26432d695b038
Carlos <nashlasterby@gmail.com>
2022-11-23 16:34:37 -0300

    feat: language chg

--- dock_playwright_test.py
+++ dock_playwright_test.py
@@ -1,13 +1,33 @@
 import re
-from playwright.sync_api import Page, expect
+from playwright.sync_api import sync_playwright
+from playwright.sync_api import expect
+from datetime import date
+from time import sleep
 
-def test_dock_homepage_has_a_unica_plataforma(page: Page):
-  page.goto("https://dock.tech/")
+with sync_playwright() as p:
+  browser = p.chromium.launch(headless=False)
+  page = browser.new_page()
+  page.goto('https://dock.tech/')
 
-  expect(page).to_have_title(re.compile("Dock - Tech your business free"))
+  cookies_accept = page.locator('xpath=//*[@id="onetrust-accept-btn-handler"]')
+  cookies_accept.click()
+  sleep(1)
 
-  know_our_solutions = page.get_by_role("link", name="Conheça nossas soluções")
-  expect(know_our_solutions).to_have_attribute("href", "#nossas-solucoes")
-  # know_our_solutions.click()
+  translate_btn = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[1]')
+  translate_btn.click()
+  english_translation = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[2]/a[1]')
+  english_translation.click()
 
-  # expect(page).to_have_url(re.compile(".*nossas-solucoes"))
+  # form filling
+  page.locator('xpath=//*[@id="lb-nome"]').fill('Carlos, tester')
+  page.locator('xpath=//*[@id="PhoneNumber2"]').fill('83999999999')
+  page.locator('xpath=//*[@id="lb-email"]').fill('test@dock.com')
+  page.locator('xpath=//*[@id="lb-empresa"]').fill('Testing at Dock')
+  page.locator('xpath=//*[@id="lb-site"]').fill('http://testing.dock/')
+  # page.locator('xpath=//*[@id="lb-assunto"]').select_option('SOLUTION - BANKING')
+  # page.locator('xpath=//*[@id="lb-idioma"]').select_option('English')
+  # page.locator('xpath=//*[@id="lb-msg"]').fill('Hi, there! This is just a testing message, please don`t sue me or be angry with me!')
+  # page.locator('xpath=//*[@id="wpcf7-f29-o1"]/form/div[2]/div[2]/span/span/span/label/input').click()
+
+  sleep(3)
+  browser.close()
