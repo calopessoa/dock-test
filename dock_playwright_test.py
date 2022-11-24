f92a80a8f512eb2eb57dbf4bb7e90b1b7d174cc1
Carlos <nashlasterby@gmail.com>
2022-11-24 00:23:46 -0300

    update: function to involve all, form filling done

--- dock_playwright_test.py
+++ dock_playwright_test.py
@@ -1,32 +1,45 @@
-import re
-from playwright.sync_api import sync_playwright
-# from playwright.sync_api import expect
+from playwright.sync_api import sync_playwright, expect
 from time import sleep
 
-with sync_playwright() as p:
-  browser = p.chromium.launch(headless=False)
-  page = browser.new_page()
-  page.goto('https://dock.tech/')
 
-  cookies_accept = page.locator('xpath=//*[@id="onetrust-accept-btn-handler"]')
-  cookies_accept.click()
-  sleep(1)
+url = 'https://dock.tech/'
 
-  translate_btn = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[1]')
-  translate_btn.click()
-  english_translation = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[2]/a[1]')
-  english_translation.click()
 
-  # form filling
-  page.locator('xpath=//*[@id="lb-nome"]').fill('Carlos, tester')
-  page.locator('xpath=//*[@id="PhoneNumber2"]').fill('83999999999')
-  page.locator('xpath=//*[@id="lb-email"]').fill('test@dock.com')
-  page.locator('xpath=//*[@id="lb-empresa"]').fill('Testing at Dock')
-  page.locator('xpath=//*[@id="lb-site"]').fill('http://testing.dock/')
-  # page.locator('xpath=//*[@id="lb-assunto"]').select_option('SOLUTION - BANKING')
-  # page.locator('xpath=//*[@id="lb-idioma"]').select_option('English')
-  # page.locator('xpath=//*[@id="lb-msg"]').fill('Hi, there! This is just a testing message, please don`t sue me or be angry with me!')
-  # page.locator('xpath=//*[@id="wpcf7-f29-o1"]/form/div[2]/div[2]/span/span/span/label/input').click()
+def test_dock_homepage():
+  with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto(url)
 
-  sleep(3)
-  browser.close()
+    cookies_accept = page.locator('xpath=//*[@id="onetrust-accept-btn-handler"]')
+    cookies_accept.click()
+    sleep(1)
+
+    translate_btn = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[1]')
+    translate_btn.click()
+    english_translation = page.locator('xpath=//*[@id="menu-menu-principal"]/div/div/div[2]/a[1]')
+    english_translation.click()
+
+    form filling
+    page.locator('xpath=//*[@id="lb-nome"]').fill('Carlos tester')
+    page.locator('xpath=//*[@id="PhoneNumber2"]').fill('83999999999')
+    page.locator('xpath=//*[@id="lb-email"]').fill('test@dock.com')
+    page.locator('xpath=//*[@id="lb-empresa"]').fill('Testing at Dock')
+    page.locator('xpath=//*[@id="lb-site"]').fill('http://testing.dock/')
+    page.locator('xpath=//*[@id="lb-assunto"]').select_option('SOLUÇÃO - BANKING')
+
+    page.locator('xpath=//*[@id="lb-idioma"]').nth(0).select_option('Inglês')
+
+    page.locator('xpath=//*[@id="lb-msg"]').fill('Hi, there! This is just a testing message, please don`t sue me or be angry with me!')
+    page.locator('xpath=//*[@id="wpcf7-f29-o1"]/form/div[2]/div[2]/span/span/span/label/input').click()
+
+    btn_send = page.locator('xpath=//*[@id="wpcf7-f29-o1"]/form/div[2]/div[3]/input')
+    expect(btn_send).to_be_visible()
+    btn_send.click()
+
+    sleep(10)
+
+    thanks_msg = page.locator('xpath=//*[@id="lb-assunto"]')
+    expect(thanks_msg).to_be_visible()
+
+    browser.close()
